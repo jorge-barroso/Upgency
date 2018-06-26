c7cd18f55d5027438129e53a46e55e9e85423351
Jorge Barroso <jorge_barroso_11@hotmail.com>
2018-06-26 17:23:52 +0100

    Further progress, close to release-like

---
+++ public/javascripts/main.js
@@ -0,0 +1,38 @@
+//$('#section_1').stellar();
+
+const $html_body = $('html,body');
+
+const $section_1 = $('#section_1');
+const $section_2 = $('#section_2');
+const $section_3 = $('#section_3');
+const $section_4 = $('#section_4');
+const $section_5 = $('#section_5');
+
+const $first = $('#first')
+const $second = $('#second')
+const $third = $('#third')
+const $fourth = $('#fourth')
+const $fifth = $('#fifth')
+
+$first.click(function(){
+    scroll($section_1);
+})
+$second.click(function(){
+    console.log("clicked");
+    scroll($section_2);
+})
+$third.click(function(){
+    scroll($section_3);
+})
+$fourth.click(function(){
+    scroll($section_4);
+})
+$fifth.click(function(){
+    scroll($section_5);
+})
+
+function scroll(element) {
+    $html_body.animate({
+        scrollTop: element.offset().top
+    }, "slow");
+}
